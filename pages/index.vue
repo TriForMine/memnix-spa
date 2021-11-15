4afca2585d0da441244eac8c6596aadc5c3f121c
Yume <yume.network@protonmail.com>
2021-11-15 16:20:09 +0100

    Update text

--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,31 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
         <VuetifyLogo />
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Memnix beta !
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            The scope of thius beta is to test the functionality and algorithmic
+            features.
+            <br />The existing decks have been pre-established and aren't
+            currently open to edit.
           </p>
           <p>
-            For more information on Vuetify, check out the
+            For more information on Memnix, check out the
             <a
               href="https://vuetifyjs.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              documentation </a
+              github </a
             >.
           </p>
           <p>
-            If you have questions, please join the official
+            If you have questions, please contact us on the official
             <a
               href="https://chat.vuetifyjs.com/"
               target="_blank"
@@ -37,22 +37,24 @@
             >.
           </p>
           <p>
-            Find a bug? Report it on the github
+            Find a bug? Fill
+
             <a
               href="https://github.com/vuetifyjs/vuetify/issues"
               target="_blank"
               rel="noopener noreferrer"
               title="contribute"
             >
-              issue board </a
+              a form </a
             >.
           </p>
           <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
+            Thank you for contributing to Memnix. A number of updates are to be
+            implemented in the next few weeks. We will keep you updated on this
+            matter.
           </p>
           <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+            <em><small>&mdash; Memnix's team</small></em>
           </div>
           <hr class="my-3" />
           <a
