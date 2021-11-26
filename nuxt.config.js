273a729780a9fc9c723ff2a9843cdecda9a79576
Yume <yume.network@protonmail.com>
2021-11-26 14:56:40 +0100

    icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - memnix-spa',
-    title: 'memnix-spa',
+    titleTemplate: '%s - Memnix',
+    title: 'Memnix',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/moutmout.ico' }
     ]
   },
 
